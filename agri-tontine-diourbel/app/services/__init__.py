3f2d100418ff693948a800557b492e6b217a799b
Doundou969 <kbayelayepro@gmail.com>
2026-02-24 15:19:42 +0000

    Delete agri-tontine-diourbel directory

--- agri-tontine-diourbel/app/services/__init__.py
+++
@@ -1,4 +0,0 @@
-from app.services.wave_payment import initier_paiement_wave
-from app.services.ai_scoring import recalculer_score, est_eligible_credit
-
-__all__ = ["initier_paiement_wave", "recalculer_score", "est_eligible_credit"]
