0e25ea49a155568454b313d33433b564ad5f0286
Doundou969 <kbayelayepro@gmail.com>
2026-02-24 14:38:25 +0000

    Delete __init__.py

--- __init__.py
+++
@@ -1,6 +0,0 @@
-from app.routes.auth import auth_bp
-from app.routes.tontines import tontines_bp
-from app.routes.cotisations import cotisations_bp
-from app.routes.credits import credits_bp
-
-__all__ = ["auth_bp", "tontines_bp", "cotisations_bp", "credits_bp"]
